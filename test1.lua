82e4a683714cc1d9161659dea4a86a5a6ba6a9d0
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-07-12 03:32:07 +0700

    Update test1.lua

--- test1.lua
+++ test1.lua
@@ -64,4 +64,4 @@ getgenv().JXMysticFarmerConfig = {
     }
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jedpep/PSX/main/JXMysticFarmer.lua"))()
+loadstring(game:HttpGet("https://github.com/sxdasfvas/test/edit/main/test2.lua"))()
