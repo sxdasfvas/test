301f4f793db101423343ab8a439b161996a838ed
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-07-16 08:24:59 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,6 @@
+repeat task.wait() until game:IsLoaded()
+task.wait()
+
 script_key="LuGUCmltYxloNlDGQRWhJFUyQaemilHk";
 if game.PlaceId == 6284583030 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4d2476afae4cd2186c3449b71d52cfc5.lua"))()
