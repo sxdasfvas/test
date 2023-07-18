6f0a282ed4c0e339771351a568869b55daf6c326
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-07-18 10:42:17 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,10 @@
+repeat task.wait() until game:IsLoaded()
+task.wait(10)
+
 script_key="LuGUCmltYxloNlDGQRWhJFUyQaemilHk";
 if game.PlaceId == 6284583030 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4d2476afae4cd2186c3449b71d52cfc5.lua"))() --script pet
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkrampet.lua"))() --script check ram pet
 else
 wait(10)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkram.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkrampet.lua"))() --script check ram pet
 end
