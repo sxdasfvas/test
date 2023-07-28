30f3ba0e4b4fe398a1c54586befd36a650cbafcd
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-07-28 09:37:55 +0700

    Update rejoin.lua

--- rejoin.lua
+++ rejoin.lua
@@ -1,8 +1,5 @@
-script_key="EHktfgNvCHcooBXlDySsGBGtRZotRgON";
+wait(2000)
 
-getgenv().Settings = {
-HopMode = "Random",
-RemoveAfterTP = true,
-Refresh = 200
-    }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e097d55d5140b97239694645bd76ba15.lua"))()
+local player = game.Players.LocalPlayer
+
+player:Kick("test")
