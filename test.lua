38fbefc1b4524a6c9d8d4074a4dc6f612c7714ad
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-07-28 08:32:01 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,11 @@
+repeat task.wait() until game:IsLoaded()
+task.wait(180)
 script_key="EHktfgNvCHcooBXlDySsGBGtRZotRgON";
 
 getgenv().Settings = {
 ["Webhook"] = "https://discord.com/api/webhooks/1116896146376376391/nxCUIoP8hJUft1K44Rdv1raf1-Y92apQRC0KVzKXK1WVz6r4RHiz22ZJhtPzNIRoWSpk", -- Discord Webhook url for 10 minutes hook.
     ["Farm Speed"] = 0.30,
-    ["KickOnCollapse"] = true, -- Will kick you Daily Collapse BUT u should recheck after 6-8 hours he dont get it thats open again before u dont join 1 time
+    ["KickOnCollapse"] = false, -- Will kick you Daily Collapse BUT u should recheck after 6-8 hours he dont get it thats open again before u dont join 1 time
     ["HideChatName"] = true, -- Hide ingame name and chat
     ["FruitHop"] = false, -- If true it hopes world&server if false it do Pixel Vault.
     ["RandomPos"] = false, -- If true it changes a bit world positions but farming maybe will be bit slower.
@@ -24,7 +26,7 @@ getgenv().Settings = {
     },
     ["Performance"] = {
         ["FPS Cap"] = 55,
-        ["Disable Rendering"] = true, -- set to true to save GPU/CPU (Screen White)
+        ["Disable Rendering"] = false, -- set to true to save GPU/CPU (Screen White)
         ["Downgraded Quality"] = true
     }
 }
