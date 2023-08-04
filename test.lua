9d385c9d8f06eb5a28364ee00a346d0eee9542da
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-08-04 15:44:21 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -12,8 +12,8 @@ getgenv().Settings = {
     ["Mystic Mine"] = true, -- Mines Mystic
     ["Cyber Cavern"] = true, -- Mines Cybern
     ["Minimum Multiplier"] = {
-        ["Giant Chest"] = 99,
-        ["Other"] = 10
+        ["Giant Chest"] = 999,
+        ["Other"] = 1
     },
     ["Mailbox"] = {
         ["Enabled"] = true, -- true sends auto gems, false it dont
