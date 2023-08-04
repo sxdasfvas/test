49bedf57c4a35acb6a6da04c43c7358610f44718
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-08-05 03:22:40 +0700

    Update CollectDrops.lua

--- CollectDrops.lua
+++ CollectDrops.lua
@@ -4,10 +4,8 @@ _G.start = true;
 while wait() and _G.start == true do
     for _,orb in pairs(orbs:GetChildren()) do
         orb.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame;
-        wait();
     end    
     for _,orb in pairs(lootbags:GetChildren()) do
         orb.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame;
-        wait();
     end
 end
