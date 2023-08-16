88004a36983adb80c95624425374f46f359bd427
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2023-08-17 00:47:40 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ script_key="EHktfgNvCHcooBXlDySsGBGtRZotRgON";
 
 getgenv().Settings = {
 ["Webhook"] = "https://discord.com/api/webhooks/1116896146376376391/nxCUIoP8hJUft1K44Rdv1raf1-Y92apQRC0KVzKXK1WVz6r4RHiz22ZJhtPzNIRoWSpk", -- Discord Webhook url for 10 minutes hook.
-    ["Farm Speed"] = 0.1,
+    ["Farm Speed"] = 0.5,
     ["KickOnCollapse"] = false, -- Will kick you Daily Collapse BUT u should recheck after 6-8 hours he dont get it thats open again before u dont join 1 time
     ["HideChatName"] = true, -- Hide ingame name and chat
     ["FruitHop"] = false, -- If true it hopes world&server if false it do Pixel Vault.
