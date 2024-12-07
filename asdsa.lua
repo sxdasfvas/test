932780bdb9e70d50c411f00faf385b965a5d4003
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2024-12-08 01:51:55 +0700

    Update asdsa.lua

--- asdsa.lua
+++ asdsa.lua
@@ -14,10 +14,10 @@ getgenv().Settings = {
         },
         Serverhop = {
             ["Switch Servers"] = true,
-            ["Teleport Delay (s)"] = 1,
+            ["Teleport Delay (s)"] = 3,
             ["Add Pro Plaza Lobbies"] = false,
             ["Constant Terminal Searching"] = true,
-            ["Terminal Searches per Item"] = 1,
+            ["Terminal Searches per Item"] = 3,
             ["Save # Servers"] = 10,
         },
         Webhook = {
