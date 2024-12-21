3fa3e78ed71020d95893ec8da89bf5a2b94b6e4f
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2024-12-22 05:46:26 +0700

    Update and rename ClaimSantaGifts.lua to gscripts.lua

---
+++ gscripts.lua
@@ -0,0 +1,13 @@
+script_key = "ipXBywOqWcuQMMEBBCmVrvDBXrSRfJLd";
+_G.GDO_HOLIDAY_EVENT = true
+_G.GDO_SNOWSTORM_SURVIVAL = true
+_G.GCONVERT_SNOWFLAKES = true
+_G.GDO_MEGA_PRESENT = false
+_G.GGFX_MODE = 1
+_G.GZONE_TO = 999
+_G.GPOTIONS = {"Coins","Lucky","The Cocktail","Treasure Hunter","Walkspeed","Diamonds","Damage"}
+_G.GHATCH_CHARGED_EGGS = true
+_G.GENCHANTS = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Coins", "Coins", "Fortune"}
+_G.GWEBHOOK_USERID = "1167566243969126442"
+_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1311053918716825720/Vi7JJ2QrcUtkNdGixBbwBWLl-uyhAzj2opmljacNj1hAML808QkEmcj2U5sPelLG475s"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
