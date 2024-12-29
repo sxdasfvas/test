79441567f91f0236df3d8446bb3b71db5aff0c40
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2024-12-29 23:08:26 +0700

    Update gscripts.lua

--- gscripts.lua
+++ gscripts.lua
@@ -1,36 +1,27 @@
 repeat wait(10) until game:IsLoaded()
-task.spawn(function()
-script_key = "ipXBywOqWcuQMMEBBCmVrvDBXrSRfJLd";
-_G.GDO_HOLIDAY_EVENT = true
-_G.GDO_SNOWSTORM_SURVIVAL = true
-_G.GCONVERT_SNOWFLAKES = true
-_G.GGFX_MODE = 1
-_G.GZONE_TO = 99
-_G.GPOTIONS = {"Coins","Lucky","The Cocktail","Treasure Hunter","Walkspeed","Diamonds","Damage"}
-_G.GHATCH_CHARGED_EGGS = false
-_G.GENCHANTS = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Criticals", "Treasure Hunter", "Treasure Hunter"}
-_G.GWEBHOOK_USERID = "1167566243969126442"
-_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1311053918716825720/Vi7JJ2QrcUtkNdGixBbwBWLl-uyhAzj2opmljacNj1hAML808QkEmcj2U5sPelLG475s"
-_G.GMAIL_RECEIVERS = {"Pr4m0t"}
-_G.GMAIL_ITEMS = {
-  ["All Huges"] = {Class = "Pet", Id = "All Huges", Amount = 1},
-  ["Snowflake Gifts"] = {Class = "Lootbox", Id = "Snowflake Gift", MinAmount = 400},
-  ["2024 New Years Gift"] = {Class = "Lootbox", Id = "2024 New Years Gift", MinAmount = 1},
-  ["Snowglobe eggs"] = {Class = "Egg", Id = "Exclusive Egg 40", MinAmount = 1},
-  ["Daycare egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
-  ["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
-  ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
-  ["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
-  ["Secret pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
+getgenv().Config = {
+    ["EquipEnchants"] = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Criticals", "Treasure Hunter", "Treasure Hunter"},
+    ["SnowstormSurvival"] = true,
+    ["Mailing"] = {
+        ["Usernames"] = {"Pr4m0t"},
+        ["Items"] = {
+            ["Misc"] = {
+                ["Lockpick A"] = 25,
+                ["Lockpick B"] = 10,
+                ["Lockpick C"] = 3,
+            },
+            ["Lootbox"] = {
+                ["Jolly Gift"] = 5,
+            },
+            ["Egg"] = {
+                ["Huge Machine Egg 4"] = 1,
+            },
+        },
+        ["MailAllHuges"] = true --// False will mail all new huges, True will send all huges in inventory
+    },
+    ["Notifications"] = {
+        ["UserID"] = "1167566243969126442",
+        ["Webhook"] = "https://discord.com/api/webhooks/1311053918716825720/Vi7JJ2QrcUtkNdGixBbwBWLl-uyhAzj2opmljacNj1hAML808QkEmcj2U5sPelLG475s"
+    }
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
-end)
-task.spawn(function()
-    task.wait(30)
-local StarterGui = game:GetService('StarterGui')
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-
-game:GetService('StarterGui'):SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
-end)
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b883bc159a5f609adb4871db6fc15ea8.lua"))()
