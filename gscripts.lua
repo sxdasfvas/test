827f7fdf91377bd92561cc0bf3c932c4eab84b9b
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2024-12-29 11:06:06 +0700

    Update gscripts.lua

--- gscripts.lua
+++ gscripts.lua
@@ -14,12 +14,7 @@ _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1311053918716825720/Vi7JJ2Q
 _G.GMAIL_RECEIVERS = {"Pr4m0t"}
 _G.GMAIL_ITEMS = {
   ["All Huges"] = {Class = "Pet", Id = "All Huges", Amount = 1},
-  ["Snowflake Gifts"] = {Class = "Lootbox", Id = "Snowflake Gift", MinAmount = 200},
-  ["2024 Gargantuan Christmas Present"] = {Class = "Lootbox", Id = "2024 Gargantuan Christmas Present", MinAmount = 1},
-  ["2024 X-Large Christmas Present"] = {Class = "Lootbox", Id = "2024 X-Large Christmas Present", MinAmount = 10},
-  ["2024 Large Christmas Present"] = {Class = "Lootbox", Id = "2024 Large Christmas Present", MinAmount = 50},
-  ["2024 Medium Christmas Present"] = {Class = "Lootbox", Id = "2024 Medium Christmas Present", MinAmount = 100},
-  ["2024 Small Christmas Present"] = {Class = "Lootbox", Id = "2024 Small Christmas Present", MinAmount = 300},
+  ["Snowflake Gifts"] = {Class = "Lootbox", Id = "Snowflake Gift", MinAmount = 400},
   ["2024 New Years Gift"] = {Class = "Lootbox", Id = "2024 New Years Gift", MinAmount = 1},
   ["Snowglobe eggs"] = {Class = "Egg", Id = "Exclusive Egg 40", MinAmount = 1},
   ["Daycare egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
@@ -27,7 +22,6 @@ _G.GMAIL_ITEMS = {
   ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
   ["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
   ["Secret pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
-  ["Mega Lucky Gingerbread"] = {Class = "Misc", Id = "Mega Lucky Gingerbread", MinAmount = 1},
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
 end)
