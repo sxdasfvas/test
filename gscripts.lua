c2a345c6986df35c94ba9a8774a97ee15f49a98d
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2024-12-30 14:18:05 +0700

    Update gscripts.lua

--- gscripts.lua
+++ gscripts.lua
@@ -14,11 +14,21 @@ _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1311053918716825720/Vi7JJ2Q
 _G.GMAIL_RECEIVERS = {"Pr4m0t"}
 _G.GMAIL_ITEMS = {
   ["All Huges"] = {Class = "Pet", Id = "All Huges", Amount = 1},
-  ["Snowflake Gifts"] = {Class = "Lootbox", Id = "Snowflake Gift", MinAmount = 400},
-  ["Lockpick A"] = {Class = "Misc", Id = "Lockpick A", MinAmount = 10},
-  ["Lockpick B"] = {Class = "Misc", Id = "Lockpick B", MinAmount = 5},
-  ["Lockpick C"] = {Class = "Misc", Id = "Lockpick C", MinAmount = 2},
+  ["Cove Lockpick"] = {Class = "Misc", Id = "Lockpick A", MinAmount = 10},
+  ["Gold Lockpick"] = {Class = "Misc", Id = "Lockpick B", MinAmount = 2},
+  ["Diamond Lockpick"] = {Class = "Misc", Id = "Lockpick C", MinAmount = 1},
+  ["Normals"] = {Class = "Pet", Id = "Poinsettia Peacock", MinAmount = 20},
+  ["Shinies"] = {Class = "Pet", Id = "Poinsettia Peacock", MinAmount = 5, IsShiny = true},
+  ["Goldens"] = {Class = "Pet", Id = "Poinsettia Peacock", MinAmount = 10, IsGold = true},
+  ["Shiny Goldens"] = {Class = "Pet", Id = "Poinsettia Peacock", MinAmount = 5, IsGold = true, IsShiny = true},
+  ["RBs"] = {Class = "Pet", Id = "Poinsettia Peacock", MinAmount = 15, IsRainbow = true},
+  ["Shiny RBs"] = {Class = "Pet", Id = "Poinsettia Peacock", MinAmount = 5, IsRainbow = true, IsShiny = true},
+  ["Snowglobe eggs"] = {Class = "Egg", Id = "Exclusive Egg 40", MinAmount = 1},
   ["Daycare egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
+  ["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
+  ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
+  ["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
+  ["Secret pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
 end)
