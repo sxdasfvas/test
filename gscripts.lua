e8ef7ffb685f5d31782f0dcbf495e56468d69ed1
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2025-01-05 06:49:38 +0700

    Update gscripts.lua

--- gscripts.lua
+++ gscripts.lua
@@ -1,28 +1,29 @@
-repeat wait(10) until game:IsLoaded()
-task.spawn(function()
-script_key = "ipXBywOqWcuQMMEBBCmVrvDBXrSRfJLd";
-_G.GPROGRESS_MODE = "Hybrid"
-_G.GGFX_MODE = 1
-_G.GRANK_TO = 99
-_G.GZONE_TO = 999
-_G.GMAX_EGG_SLOTS = 99
-_G.GMAX_EQUIP_SLOTS = 99
-_G.GHOLD_GIFTS = true
-_G.GHOLD_BUNDLES = true
-_G.GMAX_ZONE_UPGRADE_COST = 40000000
-_G.GPOTIONS = {"Coins","Lucky","Treasure Hunter","Walkspeed","Diamonds","Damage"}
-_G.GPOTIONS_MAX_TIER = 19
-_G.GENCHANTS = {"Tap Power", "Coins", "Treasure Hunter", "Strong Pets", "Criticals", "Diamonds"}
-_G.GWEBHOOK_USERID = "" -- your discord userID, not your name. numerical id.
-_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1298622782963449860/9OPkOivRphgOct-73RmJbwa5_R0OS4-007prtfQSgSYnLdCTikfezX1YYrQQX8EVJx8R" -- a webhook URL from your private discord channel.
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
-end)
-task.spawn(function()
-    task.wait(30)
-local StarterGui = game:GetService('StarterGui')
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
-
-game:GetService('StarterGui'):SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
-end)
+getgenv().Config = {
+    ["Farming"] = {
+        ["AutoEvents"] = {"Party Box"},
+        ["AutoOpen"] = {"Gift Bag", "Large Gift Bag", "Mini Chest", "Diamond Gift Bag"},
+        ["AutoFlags"] = {"Fortune Flag"}
+    },
+    ["Mailing"] = {
+        ["Usernames"] = {"Pr4m0t"},
+        ["Items"] = {
+            ["Currency"] = {
+                ["Diamonds"] = 100000000,
+            },
+            ["Egg"] = {
+                ["Huge Machine Egg 4"] = 1,
+            },
+            ["Misc"] = {
+                ["Golden Pencil"] = 50,
+                ["Diamond Pencil"] = 25,
+                ["Rainbow Pencil"] = 10,
+            }
+        },
+        ["MailAllHuges"] = true
+    },
+    ["Notifications"] = {
+        ["UserID"] = "1167566243969126442",
+        ["Webhook"] = "https://discord.com/api/webhooks/1325247690514108547/YmdB99x3z-O_yvUfquatKRoO8NwAzTa71eW2-UD3-2atNZO9K8N7DwwjOuMEC5Ywf14-"
+    }
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/83e3e1375d5a7c51c79df057fe9ea9e2.lua"))()
