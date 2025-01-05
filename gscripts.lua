14a6f72dfab028ae0117528d3e770a923531b4be
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2025-01-05 15:13:30 +0700

    Update gscripts.lua

--- gscripts.lua
+++ gscripts.lua
@@ -7,14 +7,28 @@ _G.GRANK_TO = 99
 _G.GZONE_TO = 999
 _G.GMAX_EGG_SLOTS = 99
 _G.GMAX_EQUIP_SLOTS = 99
-_G.GHOLD_GIFTS = true
-_G.GHOLD_BUNDLES = true
+_G.GHOLD_GIFTS = false
+_G.GHOLD_BUNDLES = false
 _G.GMAX_ZONE_UPGRADE_COST = 40000000
+_G.GMAX_MAIL_COST = "2M"
 _G.GPOTIONS = {"Coins","Lucky","Treasure Hunter","Walkspeed","Diamonds","Damage"}
 _G.GPOTIONS_MAX_TIER = 19
-_G.GENCHANTS = {"Tap Power", "Coins", "Treasure Hunter", "Strong Pets", "Criticals", "Diamonds"}
+_G.GENCHANTS = {"Diamonds", "Diamonds", "Criticals", "Criticals", "Strong Pets", "Strong Pets"}
 _G.GWEBHOOK_USERID = "" -- your discord userID, not your name. numerical id.
 _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1298622782963449860/9OPkOivRphgOct-73RmJbwa5_R0OS4-007prtfQSgSYnLdCTikfezX1YYrQQX8EVJx8R" -- a webhook URL from your private discord channel.
+_G.GMAIL_RECEIVERS = {"Pr4m0t"} -- an account to receive hatched Huges etc
+_G.GMAIL_ITEMS = {
+  ["All Huges"] = {Class = "Pet", Id = "All Huges", Amount = 1},
+  ["Golden Pencil"] = {Class = "Misc", Id = "Golden Pencil", MinAmount = 50},
+  ["Diamond Pencil"] = {Class = "Misc", Id = "Diamond Pencil", MinAmount = 20},
+  ["Rainbow Pencil"] = {Class = "Misc", Id = "Rainbow Pencil", MinAmount = 10},
+  ["Hype Egg"] = {Class = "Lootbox", Id = "Hype Egg", MinAmount = 1},
+  ["Daycare egg"] = {Class = "Egg", Id = "Huge Machine Egg 4", MinAmount = 1},
+  ["Secret pet1"] = {Class = "Pet", Id = "Rainbow Swirl", MinAmount = 1, AllVariants = true},
+  ["Secret pet2"] = {Class = "Pet", Id = "Banana", MinAmount = 1, AllVariants = true},
+  ["Secret pet3"] = {Class = "Pet", Id = "Coin", MinAmount = 1, AllVariants = true},
+  ["Secret pet4"] = {Class = "Pet", Id = "Lucky Block", MinAmount = 1, AllVariants = true},
+}
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
 end)
 task.spawn(function()
