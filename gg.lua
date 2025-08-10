570b5651d85463d4ffd10812c86a1604bebde041
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2025-08-10 16:31:59 +0700

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -15,7 +15,7 @@ getgenv().ConfigsKaitun = {
 	Collect_Cooldown = 120, -- cooldown to collect fruit
 
 	["Low Cpu"] = true,
-	["Auto Rejoin"] = true,
+	["Auto Rejoin"] = false,
 
 	["Rejoin When Update"] = false,
 	["Limit Tree"] = {
