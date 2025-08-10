524362a438840f1c9b55c8bb665cdc6453ae76a8
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2025-08-10 21:39:35 +0700

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -8,51 +8,62 @@ print('this is loadstring for grow a garden')
 getgenv().ConfigsKaitun = {
 	Beta_Fix_Data_Sync = true,
 
-	NoDeletePlayer = false,
-
 	["Block Pet Gift"] = true,
 
-	Collect_Cooldown = 120, -- cooldown to collect fruit
+	Collect_Cooldown = 60, -- cooldown to collect fruit
+	JustFuckingCollectAll = false, -- Collect all (fruit not wait mutation)
 
 	["Low Cpu"] = true,
 	["Auto Rejoin"] = true,
 
 	["Rejoin When Update"] = false,
 	["Limit Tree"] = {
-		["Limit"] = 250,
-		["Destroy Untill"] = 200,
+		["Limit"] = 250
+		["Destroy Untill"] = 200
 
 		["Safe Tree"] = {
-			"Tranquil Bloom",
-			"Maple Apple",
-			"Moon Mango",
+			["Tomato"] = 1,
+			["Blood Banana"] = 1,
+			["Bamboo"] = 1,
+			["Mango"] = 1,
+			["Pineapple"] = 1,
+			["Beanstalk"] = 1,
+			["Giant Pinecone"] = 1,
+			["Sugar Apple"] = 1,
+			["Pepper"] = 1,
+			["Cacao"] = 1,
+			["Elder Strawberry"] = 1,
+			["Apple"] = 1,
 			"Bone Blossom",
-			"Fossilight",
-      		"Serenity",
-
+			"Grand Tomato",
+			["Ember Lily"] = 1,
+			["Violet Corn"] = 1,
+			["Pumpkin"] = 1,
+			["Moon Melon"] = 1,
+			["Apple"] = 1,
+			["Coconut"] = 1,
+			["Feijoa"] = 1,
+			["Crown Melon"] = 1,
+			["Grape"] = 1,
+			["Dragon Fruit"] = 1,
+			["Cactus"] = 1,
+			["Peach"] = 1,
+			["Corn"] = 1,
+			["Banana"] = 2
+			["Serenity"] = 1,
+			["Moon Mango"] = 1,
+			["Maple Apple"] = 1,
+			["Hive Fruit"] = 1,
+			["Lilac"] = 1,
+			["Soft Sunshine"] = 1,
 		}
 	},
 
 	Seed = {
 		Buy = {
-			Mode = "Custom", -- Custom , Auto
-			Custom = {
-				"Tomato",
-				"Strawberry",
-				"Bell Pepper",
-				"Blood Banana",
-				"Onion",
-				"Pear",
-				"Grape",
-				"Mushroom",
-				"Pepper",
-				"Cacao",
-				"Beanstalk",
-				"Ember Lily",
-				"Sugar Apple",
-				"Burning Bud",
-				"Giant Pinecone",
-				"Elder Strawberry",
+			Mode = "Auto", -- Custom , Auto
+			Custom = { -- any fruit u need to place
+				"Carrot",
 			}
 		},
 		Place = {
@@ -61,12 +72,6 @@ getgenv().ConfigsKaitun = {
 				"Carrot"
 			},
 			Lock = {
-				"Sunflower",
-				"Dragon Pepper",
-				"Elephant Ears",
-				"Moon Melon",
-				"Moon Mango",
-				"Fossilight",
 			}
 		}
 	},
@@ -79,7 +84,7 @@ getgenv().ConfigsKaitun = {
 
 	Events = {
 		["Cook Event"] = {
-			Minimum_Money = 30_000_000, -- minimum money to start play this event
+			Minimum_Money = 10_000_000, -- minimum money to start play this event
 			Rewards_Item = { -- The top is the most top mean prefered.
 				"Gorilla Chef",
 				"Gourmet Egg",
@@ -96,15 +101,9 @@ getgenv().ConfigsKaitun = {
 			"Anti Bee Egg",
 		},
 		Shop = {
-			"Zen Egg",
-			"Zen Seed Pack",
 			"Spiked Mango",
-			"Pet Shard Tranquil",
-			"Pet Shard Corrupted",
-			"Koi",
-			"Soft Sunshine",
-			"Sakura Bush",
-			"Raiju",
+			"Bell Pepper",
+			"Feijoa",
 		},
 		Start_Do_Honey = 2_000_000 -- start trade fruit for honey at money
 	},
@@ -141,9 +140,6 @@ getgenv().ConfigsKaitun = {
 			"Bug Egg",
 			"Paradise Egg",
 			"Bee Egg",
-			"Rare Summer Egg",
-			"Mythical Egg",
-			"Common Summer Egg",
 		},
 		Buy = {
 			"Bee Egg",
@@ -154,34 +150,29 @@ getgenv().ConfigsKaitun = {
 			"Rare Summer Egg",
 			"Bug Egg",
 			"Mythical Egg",
-			"Common Summer Egg",
-			"Common Egg",
 		}
 	},
 
 	Pets = {
-		["Start Delete Pet At"] = 45,
+		["Start Delete Pet At"] = 40,
 		["Upgrade Slot"] = {
 			["Pet"] = {
-				["Starfish"] = { 5, 100, 1, true }, -- the "true" on the last is auto equip (use for like only need to use for upgrade pet)
+				["Starfish"] = { 8, 100, 1, true }, -- the "true" on the last is auto equip (use for like only need to use for upgrade pet)
 			},
-			["Limit Upgrade"] = 2, -- max is 5 (more than or lower than 1 will do nothing)
+			["Limit Upgrade"] = 2,
 			["Equip When Done"] = {
-				["Tanchozuru"] = { 5, 100, 1 }, -- 5 on the first mean equip only 5 | pet , 100 mean equip only level pet lower than 100 | the one on the last is priority it will ues first if possible 
-				["Ostrich"] = { 3, 100, 2 },
-				["Blood Kiwi"] = { 8, 100 },
-				["Seal"] = { 8, 100 },
-				["Rooster"] = { 8, 100 },
-				["Starfish"] = { 5, 75 },
-				["Spaghetti Sloth"] = { 4, 100 },
-				["Koi"] = { 2, 100 },
+				["Gorilla Chef"] = { 8, 100, 1 },
+				["Sunny-Side Chicken"] = { 8, 100, 3 },
+				["Blood Kiwi"] = { 8, 100, 2 },
+				["Rooster"] = { 8, 100, 4 },
+				["Chicken"] = { 8, 100, 5 },
 			},
 		},
 		Favorite_LockedPet = true,
-		Locked_Pet_Age = 60, -- pet that age > 60 will lock
+		Locked_Pet_Age = 80, -- pet that age > 80 will lock
 		Locked = {
 			"French Fry Ferret",
-			"Spaghetti Sloth",
+			"Lobster Thermidor",
 			"Corrupted Kitsune",
 			"Raiju",
 			"Koi",
@@ -207,13 +198,13 @@ getgenv().ConfigsKaitun = {
 			"Blood Owl",
 			["Starfish"] = 10,
 		},
-		LockPet_Weight = 5, -- if Weight >= 10 they will locked
+		LockPet_Weight = 7, -- if Weight >= 10 they will locked
 	},
 
 	Webhook = {
-        UrlPet = "",
-        UrlSeed = "",
-        PcName = "SUPERCOMPUTER-NASA-888",
+		UrlPet = "xxx",
+		UrlSeed = "xxx",
+		PcName = "xxx",
 
 		Noti = {
 			Seeds = {
