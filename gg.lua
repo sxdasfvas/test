30131df9966378d081f783b308144a9226ed9f6b
sxdasfvas <138714819+sxdasfvas@users.noreply.github.com>
2025-08-18 20:53:40 +0700

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -377,4 +377,4 @@ Events = {
 }
 
 License = getgenv().KeyAYA
-loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/98vt13x1h979yxngcvxbkjsq8hsw9rpw.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
